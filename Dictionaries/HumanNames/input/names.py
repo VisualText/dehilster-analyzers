be9f56813e1ace7bb76fcf8dd0dc1c091456cfa4
David de Hilster <david.dehilster@lexisnexisrisk.com>
2023-05-15 23:23:30 -0400

    DEHILSTER-ANA-006 Updated python script for names
    Signed-off-by: David de Hilster <david.dehilster@lexisnexisrisk.com>

--- Dictionaries/HumanNames/input/names.py
+++ Dictionaries/HumanNames/input/names.py
@@ -1,16 +1,34 @@
 import urllib.request
 import os
-import codecs
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
-import re
 
-def fetch_dictionary(num, lang, gender, abbrev, charflag):
+def sort_and_remove_duplicates(input_file, output_file):
+    lines = set()  # Use a set to store unique lines
+    
+    # Read input file and collect unique lines
+    with open(input_file, 'r', encoding="utf-8") as file:
+        for line in file:
+            lines.add(line.strip())
+        file.close()
+
+    # Sort the lines alphabetically
+    sorted_lines = sorted(lines)
+    
+    # Write sorted lines to the output file
+    with open(output_file, 'w', encoding="utf-8") as file:
+        file.write('\n'.join(sorted_lines))
+        file.close()
+
+def fetch_gender(num, lang, gender, abbrev):
     urlbase = f"https://www.behindthename.com/submit/names/gender/{gender}/usage/{lang}/"
-    dict = f"{abbrev}-names.dict"
+    txt = f"{abbrev}-names.txt"
 
-    filename = os.path.join(os.path.dirname(__file__), dict)
+    filename = os.path.join(os.path.dirname(__file__), txt)
     file1 = open(filename, "a", encoding="utf-8")
+    biglang = lang.capitalize();
+    header = f"# {biglang} first names with gender\n"
+    file1.write(header)
     
     end = num + 1
     for i in range(1,end):
@@ -49,8 +67,10 @@ def fetch_dictionary(num, lang, gender, abbrev, charflag):
                     break
 
                 dictline = f"{name} name=first gender={gender}"
-                if charflag != 0:
+                if len(char) > 0:
                     dictline = dictline + f" {lang}=\"{char}\""
+                else:
+                    dictline = dictline + f" {lang}=1"
                 dictline = dictline + "\n"
 
                 print(dictline.strip())
@@ -58,11 +78,21 @@ def fetch_dictionary(num, lang, gender, abbrev, charflag):
 
     file1.close()
 
-fetch_dictionary(13,'chinese','feminine','zh',1)
-fetch_dictionary(5,'chinese','masculine','zh',1)
-fetch_dictionary(14,'japanese','feminine','jp',1)
-fetch_dictionary(13,'japanese','masculine','jp',1)
-fetch_dictionary(10,'spanish','feminine','sp',0)
-fetch_dictionary(9,'spanish','masculine','sp',0)
-fetch_dictionary(8,'portuguese','feminine','pt',0)
-fetch_dictionary(7,'portuguese','masculine','pt',0)
+def fetch_dictionary(fem,masc,language,abbrev):
+    fetch_gender(fem,language,'feminine',abbrev)
+    fetch_gender(masc,language,'masculine',abbrev)
+
+    txt = f"{abbrev}-names.txt"
+    filename = os.path.join(os.path.dirname(__file__), txt)
+    dict = f"{abbrev}-names.dict"
+    dictname = os.path.join(os.path.dirname(__file__), dict)
+    sort_and_remove_duplicates(filename,dictname)
+    os.remove(filename)
+
+fetch_dictionary(13,5,'chinese','zh')
+fetch_dictionary(14,13,'japanese','jp')
+fetch_dictionary(10,9,'spanish','sp')
+fetch_dictionary(8,7,'portuguese','pt')
+fetch_dictionary(2,2,'tamil','ta')
+fetch_dictionary(3,3,'hindi','hi')
+fetch_dictionary(1,1,'nepali','ne')
