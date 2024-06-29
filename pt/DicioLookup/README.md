dbbc2829261e5adc6d2773adcf47fc6f71384710
dehilsterlexis <david.dehilster@lexisnexisrisk.com>
2024-06-28 20:06:24 -0400

    DEHILSTER-ANALYZER-028 New pt analyzers
    Signed-off-by: dehilsterlexis <david.dehilster@lexisnexisrisk.com>

---
+++ pt/DicioLookup/README.md
@@ -0,0 +1,3 @@
+# DICIO WEBSITE LOOKUP
+
+https://www.dicio.com.br/deferido/
---
+++ pt/DicioLookup/input/fetchPages.py
@@ -0,0 +1,73 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+import unicodedata
+
+def fullpath(file_name, subdir=""):
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, subdir, file_name)
+    # file_path = os.path.join(current_directory, "pt", "DicioLookup", "input", subdir, file_name)
+    return file_path
+
+def remove_accents(input_string):
+    nfkd_form = unicodedata.normalize('NFKD', input_string)
+    return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+# Read the list of words from a text file (assuming the file is named "words.txt")
+
+input_file = fullpath("words.txt")
+with open(input_file, "r", encoding="utf-8") as file:
+    words = file.read().splitlines()
+
+# Base URL to create the search URLs (replace with an actual search engine URL)
+base_url = "https://www.dicio.com.br/"
+
+# Initialize a list to store the extracted content
+extracted_content = []
+counter = 0
+good_counter = 0
+orphan_counter = 0
+wordcount = len(words)
+
+orphan_file = fullpath("orphans.txt")
+orphans = []
+if os.path.exists(orphan_file):
+    with open(orphan_file, "r", encoding="utf-8") as file:
+        orphans = file.read().splitlines()
+orphan_counter = len(orphans)
+
+# Iterate over each word
+for word in words:
+    counter += 1
+    output_file = fullpath(f"{word}.txt","words")
+    if os.path.exists(output_file):
+        good_counter += 1
+        continue
+    if word in orphans:
+        continue
+    print(f"Downloading {good_counter}/{orphan_counter}/{counter}/{wordcount} '{word}'...", end=" ")
+
+    # Create the search URL
+    search_url = base_url + remove_accents(word)
+
+    # Send a GET request to the search URL
+    response = requests.get(search_url)
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Find the div with class "content-card" (adjust the class name as needed)
+    content_card_div = soup.find("div", class_="card-main")
+
+    if content_card_div:
+        good_counter += 1
+        print("")
+        with open(output_file, "w", encoding="utf-8") as f:
+            html = content_card_div.prettify()
+            f.write(word + "\n")
+            f.write(html)
+    else:
+        orphan_counter += 1
+        print(f"   ===> not in Dicio...")
+        with open(orphan_file, "a", encoding="utf-8") as f:
+            f.write(word + "\n")
